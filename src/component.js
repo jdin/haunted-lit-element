6fec92bd9a5a1c4e03e637b3cdd09acdb1690033
Sergey Kondratenko <Sergey.Kondratenko@ing.com>
2019-09-12 15:38:40 +0200

    maker properties argument more generic to support other LitElement mixins

--- src/component.js
+++ src/component.js
@@ -1,16 +1,15 @@
 import { HauntedLitElement } from './HauntedLitElement.js';
 
-export const litElementComponent = (renderer, props = {}, baseCls = HauntedLitElement) =>
-  class extends baseCls {
-    static get styles() {
-      return props.styles || [];
-    }
-
-    static get properties() {
-      return props.properties || {};
-    }
-
+export const litElementComponent = (renderer, props = {}, baseCls = HauntedLitElement) => {
+  const retCls = class extends baseCls {
     render() {
       return renderer.call(this, this);
     }
   };
+
+  Object.entries(props).forEach(([key, val]) => {
+    retCls[key] = val;
+  });
+
+  return retCls;
+};

967b2fc156702c5c3d5241e97a75b1322a314c59
Sergey Kondratenko <Sergey.Kondratenko@ing.com>
2019-10-01 12:26:01 +0200

    pass 'this' reference to State object

--- src/HauntedLitElement.js
+++ src/HauntedLitElement.js
@@ -4,7 +4,7 @@ import { State } from 'haunted/core.js';
 export class HauntedLitElement extends LitElement {
   constructor() {
     super();
-    this.haunted = new State(() => this.requestUpdate());
+    this.haunted = new State(() => this.requestUpdate(), this);
   }
 
   update(_changedProperties) {
